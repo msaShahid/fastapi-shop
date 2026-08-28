467ce289c22c21daec227a3eb66187de1b0ee3d5
msaShahid <msa.ansari28@gmail.com>
2026-08-28 13:36:40 +0530

    feat(auth): add new exception classes for authentication errors
    - Introduced `InvalidRefreshTokenError` and `revoke_refresh_token` exception classes to improve error handling in the authentication module.
    - Added a placeholder for `InvalidCredentialsError` to maintain consistency in exception management.

--- app/modules/auth/exceptions/auth_exceptions.py
+++ app/modules/auth/exceptions/auth_exceptions.py
@@ -8,3 +8,9 @@ class UsernameAlreadyExistsError(Exception):
 
 class InvalidCredentialsError(Exception):
     pass
+
+class InvalidRefreshTokenError(Exception):
+    pass
+
+class revoke_refresh_token(Exception):
+    pass
