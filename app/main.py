5b4169af883b61355987ecc4ea03d30f84eb2c4a
msaShahid <msa.ansari28@gmail.com>
2026-08-29 11:30:49 +0530

    feat(main): integrate category and product routers into the main application
    - Added `category_router` and `product_router` to the main application, enabling category and product-related API endpoints under the specified API version prefix.
    - This integration enhances the modularity and functionality of the application by centralizing category and product management within their respective modules.

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.config import get_settings
 from app.core.database import get_db
 from app.modules.auth.api.router import auth_router
+from app.modules.categories.api.router import category_router
 from app.modules.playground.api.router import playground_router
+from app.modules.products.api.router import product_router
 from app.modules.users.api.router import users_router
 
 settings = get_settings()
@@ -21,6 +23,8 @@ app = FastAPI(
 app.include_router(playground_router, prefix=settings.api_v1_prefix)
 app.include_router(auth_router, prefix=settings.api_v1_prefix)
 app.include_router(users_router, prefix=settings.api_v1_prefix)
+app.include_router(category_router, prefix=settings.api_v1_prefix)
+
 
 
 @app.get("/health", tags=["health"])
