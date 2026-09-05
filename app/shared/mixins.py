e5ba80ae8b89fa77f5909ab7e10df4de4053d360
msaShahid <msa.ansari28@gmail.com>
2026-09-05 16:23:36 +0530

    feat(mixins): enable eager defaults for timestamps in TimestampMixin
    - Added `__mapper_args__` with `eager_defaults` set to `True` in the `TimestampMixin` class to ensure default values are eagerly loaded for created and updated timestamps.
    - This change enhances the behavior of timestamp management in the application.

--- app/shared/mixins.py
+++ app/shared/mixins.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class TimestampMixin:
 
+    __mapper_args__ = {"eager_defaults": True}
+
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
